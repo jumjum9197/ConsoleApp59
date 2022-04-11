c3789d318692473f8280203f33c196cfaaade271
Olajumoke Sekoni <sekoniolajumoke@gmail.com>
2022-04-11 14:40:01 +0100

    Add project files.

---
+++ ConsoleApp59/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LearnCSharp.Cmd
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ShoppingBag<Bag> shoppingBag = new ShoppingBag<Bag>();
+            
+       
+            var counter = 0;
+            while (counter < 5)
+            {
+                Console.WriteLine(shoppingBag.GetItem(counter));
+                counter++;
+            }
+        }
+    }
+
+    public class ShoppingBag<T> where T : Bag
+    {
+        private T[] _items = new T[5];
+        private int _index = 0;
+        public void AddItem(T item)
+        {
+            _items[_index++] = item;
+        }
+
+        public T GetItem(int index)
+        {
+            return _items[index];
+        }
+    }
+
+    public class Bag
+    {
+       
+
+
+    }
+}
